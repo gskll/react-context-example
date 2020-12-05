8fe11e9c56524501dc69ae39e699abf937b117a9
Andrew <gskll@protonmail.com>
2020-12-05 22:15:58 +0100

    feat: fetch posts/users data from api
    - fetch posts on mount and save to state.posts
    - iterate over posts and extract unique user ids
    - fetch users based on unique user ids and save to state.users

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,34 @@
 import React from "react";
 
 class App extends React.Component {
+  state = { posts: [], users: [] };
+
+  fetchPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((posts) => {
+        this.setState({ posts });
+
+        this.fetchUsers();
+      });
+  }
+
+  fetchUsers() {
+    const userIds = [...new Set(this.state.posts.map((post) => post.userId))];
+
+    userIds.forEach((userId) => {
+      fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+        .then((response) => response.json())
+        .then((user) => {
+          this.setState({ users: [...this.state.users, user] });
+        });
+    });
+  }
+
+  componentDidMount() {
+    this.fetchPosts();
+  }
+
   render() {
     return <div>App</div>;
   }
